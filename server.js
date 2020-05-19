f4622e9d7dd92a35f52b29128ad149dd9097f061
Kotth <arnaud.nilens@gmail.com>
2020-05-19 11:03:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -239,9 +239,10 @@ const login = (req, res) => {
             res.status(200).json(user);
         } else if (results[0].password !== req.body.password) {
             res.status(400).json({message : 'Mot de passe incorrect.'});
-        } else {
-            res.status(400).json({message: 'Votre nom d\'utilisateur n\'éxiste pas,\n veuillez vous inscrire.'});
         }
+
+        res.status(400).json({message: 'Votre nom d\'utilisateur n\'éxiste pas,\n veuillez vous inscrire.'});
+
     });
 }
 
